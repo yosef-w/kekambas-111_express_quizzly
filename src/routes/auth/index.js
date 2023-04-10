ec432f94c09904b010faa080c2bde9d770ac24e5
Brian <bstanton773@gmail.com>
2023-04-10 15:38:41 -0500

    Add login route and template

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -9,4 +9,13 @@ MainAuthRouter.route('/register')
     })
 
 
+MainAuthRouter.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req, res) => {
+        res.send('Post Auth Router Login')
+    })
+
+
 module.exports = MainAuthRouter
