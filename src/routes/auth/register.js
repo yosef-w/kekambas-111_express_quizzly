4ff2719b5eafb9581d4df951aa9e0389823d2121
yosef-w <ywolday24@gmail.com>
2023-04-11 14:35:31 -0500

    Add new users to dabase on register

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,20 @@
-module.exports = (req, res) => {
+const { User } = require('../../models')
+const bcrypt = require('bcrypt')
+
+module.exports = async (req, res) => {
     console.log(req.body);
-    res.send('Post Auth Router Register');
+    if (req.body.password !== req.body.confirmPass){
+        res.status(400).send({error: "Your passwords do not match"})
+    } else {
+        const { username, email, password } = req.body
+        const passwordHash = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: passwordHash })
+        try{
+            await user.save();
+            res.send(`New User Created - ${user.username}`);
+        } catch (err){
+            console.log(err);
+            res.send('There was an issue')
+        }
+    }
 }
